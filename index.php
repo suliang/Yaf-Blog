33c7d68d5288b4b3cf8a8aeceba838fe72d5804b
苏良 <cs4616301@163.com>
2015-10-20 19:26:59 +0800

    add const

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 define('APPLICATION_PATH', dirname(__FILE__));
 define('BASE_URL',"http://www.programcat.com/");
 define('CACHE_TIME', 3600);
+define('IP',"123.57.52.70");
 
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
 
