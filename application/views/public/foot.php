47ea43f187be10debf080d39ce7499ff6716ae7c
苏良 <suliang@mia.com>
2016-05-20 13:32:48 +0800

    去掉无用的请求

--- application/views/public/foot.php
+++ application/views/public/foot.php
@@ -31,9 +31,7 @@
                 <script type="text/javascript">
 
                         //document.write(unescape("%3Cspan id='cnzz_stat_icon_1255689966'%3E%3C/span%3E%3Cscript src='http://s95.cnzz.com/stat.php%3Fid%3D1255689966%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
-                        $(function(){
-                            $.get("http://www.aibj.xyz/?fromuid=468621");
-                        })
+                    
                         
                 </script>
 
