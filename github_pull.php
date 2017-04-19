4b4a088db800ddd33f6176131a2d7f614418bdf8
suliang <dream@programcat.com>
2017-04-20 02:31:34 +0800

    key

--- github_pull.php
+++ github_pull.php
@@ -5,11 +5,11 @@ $github_signa = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 list($hash_type, $hash_value) = explode('=', $github_signa, 2);
 $payload = file_get_contents("php://input");
 
-$secret = '************';
+$secret = 'programcat';
 
 $hash = hash_hmac($hash_type,$payload,$secret);
 
-if($hash == $hash_value)
+if($hash && $hash === $hash_value)
 {
 	echo '认证成功，开始更新'."\n\r";
     echo exec("./github_pull.sh");
