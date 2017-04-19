1082b2413dcf877c97d45e4ca781f7e85d4ee36d
苏良 <suliang@mia.com>
2017-04-20 00:41:52 +0800

    定义地址

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ define('APPLICATION_PATH', dirname(__FILE__));
 define('BASE_URL',"http://www.programcat.com/");
 define('CACHE_TIME', 3600);
 define('IP',"123.57.52.70"); 
-
+define('STATIC_PUBLIC', "http://www.programcat.com/");
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
