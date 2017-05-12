bd0d198ccca2c5cfd2c61e5224cc79f4f067eac1
suliang <dream@programcat.com>
2017-05-13 01:53:57 +0800

    移动端隐藏右侧标签栏和分类栏

--- application/views/public/foot.php
+++ application/views/public/foot.php
@@ -1,4 +1,4 @@
-                <div class="col-md-2" >
+                <div class="col-md-2  hidden-xs hidden-sm" >
                     <div class="row" style="padding-left:10px;">
                         <div class="col-md-12" style="background-color: white;padding-left: 5px;padding-top:10px">
                             <h4 class="text-muted">博客分类</h4>
@@ -45,28 +45,23 @@
                 <p class="text-center " style="color: white">Copyright © 程序喵的博客 | <?=$back;?></p>
             </div>
         </footer>
-
-
-
-            <!-- 登录窗口 -->
-            <div class="modal fade" id="login_page" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="myModalLabel">进入后台</h4>
-                        </div>
-                        <div class="modal-body">
-                            <input type="text" class="form-control " id="password">
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-                            <button type="button" class="btn btn-primary" id="loginpost">登录</button>
+                <!-- 登录窗口 -->
+                <div class="modal fade" id="login_page" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title" id="myModalLabel">进入后台</h4>
+                            </div>
+                            <div class="modal-body">
+                                <input type="text" class="form-control " id="password">
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                                <button type="button" class="btn btn-primary" id="loginpost">登录</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-
     </body>
 </html>
